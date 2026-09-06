d7f02d063b3b9544fc0ce1595290b428c12f40dd
Danielpatarroyo <Daniel.patarroyo.o@gmail.com>
2026-09-05 22:45:30 -0500

    Primer commit

---
+++ bin/dart_hola_mundo_prueba.dart
@@ -0,0 +1,6 @@
+import 'package:dart_hola_mundo_prueba/dart_hola_mundo_prueba.dart'
+    as dart_hola_mundo_prueba;
+
+void main(List<String> arguments) {
+  print('Hello world desde bin run: ${dart_hola_mundo_prueba.calculate()}!');
+}
---
+++ test/dart_hola_mundo_prueba_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_hola_mundo_prueba/dart_hola_mundo_prueba.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
